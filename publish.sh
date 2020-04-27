5a51b4ba0fa677703a42f6c9a004e2584d0cbb51
Jonathan H <Jhadida@fmrib.ox.ac.uk>
2020-04-27 23:37:25 +0100

    push tags

--- publish.sh
+++ publish.sh
@@ -7,5 +7,7 @@ shift
 
 PAT=$(openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 10000 -salt -d -in ".token.enc")
 
-git tag v$Version "$@"
 vsce publish -p "$PAT" "$@" $Version
+git tag v$Version "$@"
+git push --tags
+
