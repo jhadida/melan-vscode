7a5b1b3c3cd17a04d84c7bea78a6442cb290820d
Jonathan H <Jhadida@fmrib.ox.ac.uk>
2020-04-27 23:50:26 +0100

    fix publish

--- publish.sh
+++ publish.sh
@@ -7,6 +7,6 @@ shift
 
 PAT=$(openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 10000 -salt -d -in ".token.enc")
 
-git tag v$Version "$@" | { echo "Failed to create tag."; exit 1; }
+git tag v$Version "$@" || { echo "Failed to create tag."; exit 1; }
 git push --tags
 vsce publish -p "$PAT" "$@" $Version
