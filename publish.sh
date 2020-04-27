b42a646edf7cbcdfdbbbabcd211f3144f5627436
Jonathan H <Jhadida@fmrib.ox.ac.uk>
2020-04-27 23:55:19 +0100

    vsce appears to create git tags as well

--- publish.sh
+++ publish.sh
@@ -7,6 +7,5 @@ shift
 
 PAT=$(openssl enc -aes-256-cbc -md sha512 -pbkdf2 -iter 10000 -salt -d -in ".token.enc")
 
-git tag v$Version "$@" || { echo "Failed to create tag."; exit 1; }
-git push --tags
 vsce publish -p "$PAT" "$@" $Version
+git push --tags
